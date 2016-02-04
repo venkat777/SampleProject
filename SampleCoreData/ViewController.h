c0d5bf6955b59426bce6e14fddec8a7468a2340c
venkataramana <venkata.gudla@ril.com>
2016-02-04 12:02:08 +0530

    Initial Commit

---
+++ SampleCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleCoreData
+//
+//  Created by VENKATARAMANA on 04/02/16.
+//  Copyright © 2016 rjil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
